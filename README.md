904a96910ee1e4e88a2e51f69db65c52942a81e8
EduardoIsaacC <a23110190@live.ceti.mx>
2026-05-31 20:02:03 -0600

    commit4

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 #Segundo commit para que mi profe no me baje 2 puntos
 PROYECTO FINAL - SISTEMAS OPERATIVOS
+.\.venv\Scripts\activate
---
+++ agente_nlp.py
@@ -0,0 +1,66 @@
+import os
+import json
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+#Caja fuerte y llave
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=api_key)
+
+def procesar_texto_cliente(mensaje_cliente):
+    """
+    Agente 1: Interpreta el lenguaje natural usando la API REST de Gemini
+    """
+    # Elegimos el modelo rápido de Gemini
+    modelo = genai.GenerativeModel('gemini-1.5-flash')
+
+    #El prompt: Instrucciones estrictas para la IA
+    instrucciones = f"""
+    Eres el Agente 1 de ventas del negocio "Aderezos Edu".
+    Tu trabajo es leer el mensaje del cliente y extraer qué aderezos quiere comprar.
+    
+    Catálogo válido:
+    Sabores: Chipotle, Habanero.
+    Tamaños: 725g, 10oz, 7oz.
+    
+    Mensaje del cliente: "{mensaje_cliente}"
+    
+    REGLA ESTRICTA: Responde ÚNICAMENTE con un arreglo en formato JSON puro. No agregues texto antes ni después.
+    El JSON debe tener esta estructura exacta para cada artículo:
+    [
+        {{"sabor": "Chipotle o Habanero", "tamano": "725g o 10oz o 7oz", "cantidad": numero_entero}}
+    ]
+    Si no entiendes el pedido, devuelve un arreglo vacío: []
+    """
+
+    #Solicitud Gemini
+    try:
+        respuesta = modelo.generate_content(instrucciones)
+        texto_limpio = respuesta.text.strip()
+
+        # Limpiamos posibles formatos de código que la IA a veces añade
+        if texto_limpio.startswith("```json"):
+            texto_limpio = texto_limpio[7:-3]
+        elif texto_limpio.startswith("```"):
+            texto_limpio = texto_limpio[3:-3]
+            
+        lista_articulos = json.loads(texto_limpio.strip())
+        return lista_articulos
+    except Exception as e:
+        print(f"Error al interpretar la respuesta de la IA: {e}")
+        return []
+
+#  PRUEBA LOCAL 
+if __name__ == "__main__":
+    # Simulamos a un cliente escribiendo de forma natural
+    texto_prueba = "Hola, me mandas porfa dos aderezos chicos (7oz) de habanero y un chipotle grandote de 725g. Gracias."
+    
+    print(f"Mensaje original: {texto_prueba}\n")
+    print("El Agente 1 está pensando...\n")
+    
+    resultado_agente = procesar_texto_cliente(texto_prueba)
+    
+    print("=== DATO ESTRUCTURADO PARA EL MOTOR DE INFERENCIA ===")
+    print(resultado_agente)
